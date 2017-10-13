078cded08e4c9f27adf53bfd320588a877f637ba
Yutaka Tachibana <yutaka.paradigm.shift@gmail.com>
2017-10-14 00:05:44 +0900

    wip

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -1,9 +1,9 @@
 worker_processes 4
 preload_app true
 pid './unicorn.pid'
-listen '/tmp/unicorn.sock'
-#listen 8080
-stderr_path './unicorn.log'
-stdout_path './unicorn.log'
+# listen '/tmp/unicorn.sock'
+listen 8080
+# stderr_path './unicorn.log'
+# stdout_path './unicorn.log'
 
 after_fork do |m, w| after_fork_foooooo end
